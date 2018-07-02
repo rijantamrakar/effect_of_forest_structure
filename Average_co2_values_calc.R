54c36a586478c4d0a9b6e92f30a70f5bf1628c9f
Rijan Tamrakar <rijantamrakar@gmail.com>
2018-07-02 14:54:50 +0100

    average annual values function completed

--- Average_co2_values_calc.R
+++ Average_co2_values_calc.R
@@ -55,40 +55,41 @@ if(length(new.packages)) {
 lapply(required.packages, require, character.only = TRUE)
 
 #------------------------------------------------------------------------------#
-#### Timeseries flux plot annual ####
+#### Timeseries flux plot annual function ####
 #------------------------------------------------------------------------------#
 
 timeseries_avg <- function(FileName,                                            # Filepath to the data
-                            Problematic.Year,                                   # List of years when data are not good or unavailable
-                            Site,                                               # flux site name
-                            timestep)                                           # can be daily, weekly, monthly, annual
-        {                      
+                           Problematic.Year,                                    # List of years when data are not good or unavailable
+                           Site,                                                # flux site name
+                           timestep,                                            # can be daily, weekly, monthly, annual
+                           PlotData)
+{                      
         
-        data           <- read.csv(FileName)
-        data           <- dplyr::select(data, 'TIMESTAMP', 'NEE_VUT_REF', 'RECO_DT_VUT_REF', 'GPP_DT_VUT_REF')
-        names(data)    <- c('timestamp', 'nee', 'reco', 'gpp')
-        data[data == -9999] <- NA
-        data$nep       <- -data$nee
-        data           <- dplyr::filter(data, !timestamp %in% Problematic.Year)
+        data           <- read.csv(FileName)                                    # reading data
+        data           <- dplyr::select(data, 'TIMESTAMP', 
+                                        'NEE_VUT_REF', 
+                                        'RECO_DT_VUT_REF', 
+                                        'GPP_DT_VUT_REF')                       # selecting the carbon dioxide flux columns
+        names(data)    <- c('timestamp', 'nee', 'reco', 'gpp')                  # renaming columns
+        data[data == -9999] <- NA                                               # converting -9999 into NA
+        data$nep       <- -data$nee                                             # getting nep
         
+        data2           <- dplyr::filter(data, !timestamp %in% Problematic.Year) # removing problematic year
         
+        ReqdVar    <- c('nep', 'reco', 'gpp')                                   # names of variable for result output
+        average    <- round(colMeans2(as.matrix(data2[,ReqdVar]), na.rm = T), 1)                      # calculating average using MatixStats package
+        stdDev     <- round(colSds(as.matrix(data2[,ReqdVar]), na.rm = T), 1)                         # calculating standard deviation using MatixStats package
+        CoefVar    <- round(100 * stdDev/abs(average), 1)                            # calculating coefficient of variation using MatixStats package
         
-        ReqdVar    <- c('nep', 'reco', 'gpp')
-        average    <- colMeans2(as.matrix(data[,ReqdVar]))
-        stdDev     <- colSds(as.matrix(data[,ReqdVar])) 
-        CoefVar    <- round(100 * stdDev/average, 1)
-        
-        results    <- data.frame(ReqdVar, average, stdDev, CoefVar)
-        results$no.of.year <- nrow(results)
-        results$site <- Site
-        results$timestep <- timestep
-        
-        
+        results    <- data.frame(ReqdVar, average, stdDev, CoefVar)             # creating a dataframe of results
+        results$no.of.year <- nrow(data2)                                        # no of years with data available
+        results$site <- Site                                                    # Site for which statistics are calculated
+        results$timestep <- timestep                                            # timesteps (daily, weekly, annual, ) 
         
         # plot the data if asked 
         if(PlotData == 'YES') {
-                maxVal <- max(data$nep, data$reco, data$gpp, na.rm = T)
-                minVal <- min(data$nep, data$reco, data$gpp, na.rm = T)
+                maxY <- max(data$nep, data$reco, data$gpp, na.rm = T)           # maximum value for y - axis
+                minY <- min(data$nep, data$reco, data$gpp, na.rm = T)           # minimum value for y - axis
                 
                 with(data, plot(timestamp, 
                                 nep, 
@@ -96,7 +97,7 @@ timeseries_avg <- function(FileName,                                            
                                 ylab = '',
                                 typ = 'l', 
                                 xlim = c(min(data$timestamp) - 2, max(data$timestamp) + 1), 
-                                ylim = c(minVal, maxVal)
+                                ylim = c(minY, maxY)
                 )
                 )
                 with(data, points(timestamp, nep, pch = 15))
@@ -107,37 +108,45 @@ timeseries_avg <- function(FileName,                                            
                 with(data, points(timestamp, reco, pch = 17))
                 with(data, lines(timestamp, reco, pch = 17))
                 
-                text(min(data$timestamp) + 1, minVal + 50, Site)
+                text(min(data$timestamp) + 1, minY + 50, Site)
                 text(min(data$timestamp) -1.5, average[1], 'NEP', cex = 0.7)
                 text(min(data$timestamp) -1.5, average[2], 'Reco', cex = 0.7)
                 text(min(data$timestamp) -1.5, average[3], 'GPP', cex = 0.7)
-                
-                mtext(expression( "CO"[2]~ "["~ g ~ C ~ m^{-2}~ yr^{-1}~ ']'), side = 2, line = 2.2, cex = 0.8)
-        }
-        
+                mtext(expression( "CO"[2]~ "["~ g ~ C ~ m^{-2}~ yr^{-1}~ ']'), 
+                      side = 2, 
+                      line = 2.2, 
+                      cex = 0.8)
+        } # end of the plotting
         return(results)
 }
 
+#------------------------------------------------------------------------------#
+#### Timeseries flux plot calculation ####
+#------------------------------------------------------------------------------#
 sitelistwithstructuredata <- substr(list.files('main_analysis/strc.data/final_data', full.names = F), 1, 6)
-dir.data <- 'main_analysis/flux.data/ann'
-dir.out   <- 'main_analysis/flux.data/annual_flux_all_sites'
+dir.data     <- 'main_analysis/flux.data/ann'
+dir.out      <- 'main_analysis/flux.data/annual_flux_all_sites'
 data.problem <- read.csv('main_analysis/flux.data/Data_with_problem.csv')
-
+AllResults   <- NULL
+dir.out      <- 'main_analysis/flux.data/annual_flux_all_sites'
 for(j in 1:length(sitelistwithstructuredata)) {
         Site <- sitelistwithstructuredata[j]
+        FileName <- list.files(dir.data, pattern = Site, full.names = T)
         if(length(FileName) == 1) {
                 data.problem.for.site <- data.problem %>%
                         filter(site == Site) %>%
                         filter(dont_use_annual == 1)
                 
                 Problematic.Year <- data.problem.for.site$Not_good_year
-                FileName <- list.files(dir.data, pattern = Site, full.names = T)
+                PlotData <- 'NO'
+                timestep <- 'ann'
                 
-                timeseries_avg(FileName, site)     
+                Res <- timeseries_avg(FileName, Problematic.Year, Site, timestep, PlotData)  
+                AllResults <- rbind(AllResults, Res)
         }
 }
 
-
-
+dir.out <- 'main_analysis/flux.data/annual_flux_all_sites'
+write.csv(AllResults, file.path(dir.out, 'annualco2fluxesstatistics.csv'), row.names = F)
 
 
